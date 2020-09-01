ece359c5bb22fdefd4cd2e114a0768812362dfa1
Zack Shone <zackariahshone@gmail.com>
2020-08-31 22:19:34 -0700

    set up server and db files

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 const express = require('express');
+const http = require("http");
+const PORT = 8080;
+const server = http.createServer(handleRequest);
+
+
+server.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
