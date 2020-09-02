1dd51feb1563559a4d57d6a1bf65571e4fec8863
Zack Shone <zackariahshone@gmail.com>
2020-09-01 22:29:58 -0700

    building out

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,7 +2,50 @@ const express = require('express');
 const burger = require('../models/burger.js');
 const router = express.Router();
 
-router.get('/', function (req, res){
-    res.send("the home page");
+
+// Import the model (cat.js) to use its database functions.
+
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    const hbsObject = {
+      burger: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
 });
+
+router.post("/api/burger", function(req, res) {
+  burger.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burger/:id", function(req, res) {
+  const condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update(
+    {
+      sleepy: req.body.sleepy
+    },
+    condition,
+    function(result) {
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+
+    }
+  );
+});
+
+// Export routes for server.js to use.
+module.exports = router;
+console.log(router);
 
--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,29 @@
 const orm = require('../config/orm.js');
 
-const burgerQuerys = {
+const burger = {
+    all: function(cb) {
+      orm.all("burger", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+      orm.create("burger", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.update("burger", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
 
-}
+  console.log(burger);
+  
+  // Export the database functions for the controller (catsController.js).
+  module.exports = burger;
+  
 
 
 
@@ -14,4 +35,4 @@ const burgerQuerys = {
 
 
 
-module.exports = burgerQuerys;
+module.exports = burger;
